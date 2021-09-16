8632bbb6aa7bec590b7912509d414c3c194f07b0
BhavithaSomashetti <somashettybhavitha@gmail.com>
2021-09-16 16:46:51 +0530

    first commit

---
+++ MavEx/src/main/java/com/lti/demo/myApp.java
@@ -0,0 +1,8 @@
+package com.lti.demo;
+
+public class myApp {
+public String show()
+{
+	return "Hello maven";
+}
+}
---
+++ MavEx/src/main/java/com/lti/demo/testMyApp.java
@@ -0,0 +1,9 @@
+package com.lti.demo;
+
+public class testMyApp {
+public static void main(String[] args) {
+	myApp a=new myApp();
+	String str=a.show();
+	System.out.println(str);
+}
+}
